f3470f04bdb03f9bfef431f0c9be36908cb0a061
Nanda Hibatullah <nandahibatullah@gmail.com>
2023-10-18 05:07:31 +1300

    feat: link avatar and games to steam store/profile pages

--- src/components/profile-summary-card.tsx
+++ src/components/profile-summary-card.tsx
@@ -28,30 +28,34 @@ export const ProfileSummaryCard = ({
   };
 
   return (
-    <Card shadow="sm" padding="sm" radius="md" className="md:flex">
+    <Card shadow="sm" padding="sm" radius="md">
       <Group justify="center" py="xs">
         <Text fw={500}>Profile Summary</Text>
       </Group>
       <div className="grid grid-cols-2 grid-rows-3 gap-4 px-4 py-4 lg:grid-cols-3 lg:grid-rows-1">
         <DetailCard>
-          <Stack gap={1} align="center">
-            <p className="font-extrabold lg:text-[1.5rem]">
-              {playerInfo.numberOfGames}
-            </p>
-            <Text className="text-center" size="sm">
-              Games Owned
-            </Text>
-          </Stack>
+          <Card.Section className="pb-2">
+            <Stack gap={1} align="center">
+              <p className="font-extrabold lg:text-[1.5rem]">
+                {playerInfo.numberOfGames}
+              </p>
+              <Text className="text-center" size="sm">
+                Games Owned
+              </Text>
+            </Stack>
+          </Card.Section>
         </DetailCard>
         <DetailCard>
-          <Stack gap={1} align="center">
-            <p className="text-center font-extrabold lg:text-[1.5rem]">
-              {playerInfo.totalPlayTime} hours
-            </p>
-            <Text className="text-center" size="sm">
-              Total Steam Playtime
-            </Text>
-          </Stack>
+          <Card.Section className="pb-2">
+            <Stack gap={1} align="center">
+              <p className="text-center font-extrabold lg:text-[1.5rem]">
+                {playerInfo.totalPlayTime} hours
+              </p>
+              <Text className="text-center" size="sm">
+                Total Steam Playtime
+              </Text>
+            </Stack>
+          </Card.Section>
         </DetailCard>
         <DetailCard classNames="col-span-2 row-span-2 lg:col-span-1 lg:row-span-1">
           <Card.Section>
@@ -61,12 +65,14 @@ export const ProfileSummaryCard = ({
               className="relative top-[-1px]"
             />
           </Card.Section>
-          <Stack gap={1} align="center">
-            <p className="break-word text-center font-extrabold lg:text-[1.25em]">
-              {playerInfo.mostPlayedGame.name}
-            </p>
-            <Text size="sm">Most played</Text>
-          </Stack>
+          <Card.Section className="pb-2">
+            <Stack gap={1} align="center">
+              <p className="break-word text-center font-extrabold">
+                {playerInfo.mostPlayedGame.name}
+              </p>
+              <Text size="sm">Most played</Text>
+            </Stack>
+          </Card.Section>
         </DetailCard>
       </div>
     </Card>
