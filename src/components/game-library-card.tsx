3246472c2a61f9d033739cb62b083567932cd52e
Nanda Hibatullah <nandahibatullah@gmail.com>
2023-10-18 06:44:38 +1300

    refactor: format summary response to reduce page load size

--- src/components/game-library-card.tsx
+++ src/components/game-library-card.tsx
@@ -3,32 +3,31 @@ import { IconClockPlay } from "@tabler/icons-react";
 import Link from "next/link";
 import { type SteamGame } from "~/types";
 
-const gameIconResolver = (appId: number) => {
+const steamGameIconResolver = (appId: number) => {
   return `https://cdn.akamai.steamstatic.com/steam/apps/${appId}/header.jpg`;
 };
 
 export const GameLibraryCard = ({ game }: { game: SteamGame }) => {
   const urlFriendlyName = game.name.replace(" ", "+");
   const fallBackImageUrl = `https://placehold.co/250x117?text=${urlFriendlyName}`;
-  const playTimeInHours = (game.playtime_forever / 60).toFixed(1);
 
   return (
-    <Link href={`https://store.steampowered.com/app/${game.appid}`}>
+    <Link href={`https://store.steampowered.com/app/${game.appId}`}>
       <Card
-        key={game.appid}
+        key={game.appId}
         shadow="xl"
         className="items-center justify-center hover:scale-105"
       >
         <Card.Section>
           <Image
             alt="game-icon"
-            src={gameIconResolver(game.appid)}
+            src={steamGameIconResolver(game.appId)}
             fallbackSrc={fallBackImageUrl}
           />
         </Card.Section>
         <Group justify="center" className="pt-2 text-center">
           <IconClockPlay />
-          <Text size="xs">{playTimeInHours} hours</Text>
+          <Text size="xs">{game.playTime} hours</Text>
         </Group>
       </Card>
     </Link>
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,8 @@
-import { type z } from "zod";
-import { type OwnedGamesResponseSchema } from "~/server/schemas/steam-api";
 import { type RouterOutputs } from "~/utils/api";
 
-export type SteamGame = z.infer<
-  typeof OwnedGamesResponseSchema
->["response"]["games"][0];
-
 export type PlayerInfo = RouterOutputs["player"]["summary"]["playerInfo"];
+export type SteamGame = {
+  appId: number;
+  name: string;
+  playTime: number;
+};
