68607d401c4286f911678318bd1b40e2cbeb0b74
Nanda Hibatullah <nandahibatullah@gmail.com>
2023-10-18 05:08:09 +1300

    feat: show recently played games + order games by playtime

---
+++ src/components/recently-played-card.tsx
@@ -0,0 +1,62 @@
+import { Group, Stack, Image, Text, Card } from "@mantine/core";
+import { type PlayerInfo } from "~/types";
+
+const gameIconResolver = (appId: number) => {
+  return `https://cdn.akamai.steamstatic.com/steam/apps/${appId}/header.jpg`;
+};
+
+type RecentlyPlayedGame = PlayerInfo["recentlyPlayedGames"][0];
+
+export const RecentlyPlayedCard = ({
+  recentlyPlayedGames,
+}: {
+  recentlyPlayedGames: RecentlyPlayedGame[];
+}) => {
+  const DetailCard = ({
+    children,
+    classNames,
+  }: {
+    children: JSX.Element | JSX.Element[];
+    classNames?: string;
+  }) => {
+    return (
+      <Card
+        style={{ backgroundColor: "#ffc438", color: "#18181b" }}
+        className={"justify-center".concat(" ", classNames ?? "")}
+      >
+        {children}
+      </Card>
+    );
+  };
+
+  return (
+    <Card shadow="sm" padding="sm" radius="md">
+      <Group justify="center" py="xs">
+        <Text fw={500}>Recently Played Games</Text>
+      </Group>
+      <div className="grid grid-cols-2 gap-4 px-4 py-4 lg:grid-cols-3">
+        {recentlyPlayedGames.map((game, index) => (
+          <DetailCard key={index} classNames="col-span-1 lg:col-span-1">
+            <Card.Section>
+              <Image
+                alt="game-icon"
+                src={gameIconResolver(game.appId)}
+                className="relative top-[-1px]"
+              />
+            </Card.Section>
+            <Card.Section className="pb-2">
+              <Stack gap={1} align="center">
+                <p className="break-word text-center font-extrabold">
+                  {game.name}
+                </p>
+                <p className="break-word text-center text-sm">
+                  {game.recentPlayTime} hours recently
+                </p>
+              </Stack>
+            </Card.Section>
+          </DetailCard>
+        ))}
+      </div>
+    </Card>
+  );
+};
--- src/server/schemas/steam-api/index.ts
+++ src/server/schemas/steam-api/index.ts
@@ -60,3 +60,18 @@ export const ResolveVanityURLResponseSchema = z.object({
     message: z.string().optional(),
   }),
 });
+
+export const RecentlyPlayedGamesResponseSchema = z.object({
+  response: z.object({
+    total_count: z.number(),
+    games: z.array(
+      z.object({
+        appid: z.number(),
+        name: z.string(),
+        playtime_2weeks: z.number(),
+        playtime_forever: z.number(),
+        img_icon_url: z.string(),
+      }),
+    ),
+  }),
+});
